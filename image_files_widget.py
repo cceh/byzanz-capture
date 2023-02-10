1e557a49af7575aebea7964ebd2d8607c809664b
Marcel Schaeben <m.schaeben@uni-koeln.de>
2023-02-10 11:21:43 +0100

    wip

--- image_files_widget.py
+++ image_files_widget.py
@@ -3,6 +3,10 @@ from PyQt6.uic import loadUi
 
 
 class ImageFilesWidget(QWidget):
+    currentPath: str = None
+
     def __init__(self, parent=None):
         super().__init__(parent)
         loadUi('ui/image_files_widget.ui', self)
+
+
